77bb44e27486cdbad67916f95e5fa21d43c03d80
Ana-lu1sa <ana.lima810@etec.sp.gov.br>
2024-05-23 08:36:41 -0300

    SALVO

--- src/screens/Task_List.js
+++ src/screens/Task_List.js
@@ -1,19 +1,32 @@
 import React, {component} from "react"
-import {StyleSheet, View, Text} from "react-native"
+import {StyleSheet, View, Text, ImageBackground} from "react-native"
+
+import today_Image from '../../assets/imgs/today.jpg'
 
 export default class Task_List extends component{
-    // extends component mostra que a classe Task_List está herdando do component 
+    // extends component mostra que a classe Task_List está herdando do component
     render(){
         return(
-            <View>
+            <View style={styles.container}>
+            <ImageBackground source={today_Image} style={styles.background}>
+
+            </ImageBackground>
+            <View style={styles.TaskList}>
                 <Text>Lista de Tarefas</Text>
+            </View>
             </View>
         )
     }
 }
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
     container:{
         flex: 1
+    },
+    background:{
+        flex: 3
+    },
+    TaskList:{
+        flex: 7
     }
 })

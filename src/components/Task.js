f69f4a5357d6f70a4c300e3309f0fc38f4ce68d2
Ana-lu1sa <ana.lima@etec.sp.gov.br>
2024-06-21 08:39:02 -0300

    Salvo 21.06

--- src/components/Task.js
+++ src/components/Task.js
@@ -9,9 +9,7 @@ export default props => {
         <View style={styles.container}>
         <TouchableWithoutFeedback onPress={() =>alert("Olá")}>
             <View style = {styles.checkContainer}>
-                <View style = {styles.done}> 
-                <icon name = "check" size={20}></icon>
-                </View>
+                {get_check_view(props.done_at)}
             </View>
         </TouchableWithoutFeedback>
         <View>
@@ -22,6 +20,20 @@ export default props => {
      
         </View>
     )
+}
+
+function get_check_view(done_at){
+    if(done_at != null){
+    return(
+        <View style={styles.done}>
+            <icon name= "check" size={20}></icon>
+        </View>
+    )
+    } else{
+        return (
+            <View style={styles.pending}></View>
+        )
+    }
 }
 
  const styles = StyleSheet.create({
@@ -61,5 +73,6 @@ export default props => {
         width: 25,
         borderRadius: 13,
         borderWidth: 1,
+        borderColor: '#555555'
     }
  })
